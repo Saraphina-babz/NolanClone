64376617cf98269f3ba09b36c56785d7e9de41ec
Saraphina-babz <oyeyemibunmi21@gmail.com>
2024-11-08 15:11:05 +0100

    added .env file

--- NolanClone/src/firebase.js
+++ NolanClone/src/firebase.js
@@ -1,21 +1,3 @@
-// src/firebase.js
-// import { initializeApp } from 'firebase/app';
-// import { getAuth } from 'firebase/auth';
-
-// const firebaseConfig = {
-//     apiKey: "YOUR_API_KEY",
-//     authDomain: "YOUR_AUTH_DOMAIN",
-//     projectId: "YOUR_PROJECT_ID",
-//     storageBucket: "YOUR_STORAGE_BUCKET",
-//     messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-//     appId: "YOUR_APP_ID"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app);
-
-// export { auth };
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
@@ -25,15 +7,27 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBxFF7bsCfHUBd7hX1nVcHw4o0y_WgSgvU",
+//   authDomain: "nolan-clone.firebaseapp.com",
+//   projectId: "nolan-clone",
+//   storageBucket: "nolan-clone.appspot.com",
+//   messagingSenderId: "175935107884",
+//   appId: "1:175935107884:web:7ad41df4eb6851f5abf289",
+//   measurementId: "G-GB84NEB95P"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyBxFF7bsCfHUBd7hX1nVcHw4o0y_WgSgvU",
-  authDomain: "nolan-clone.firebaseapp.com",
-  projectId: "nolan-clone",
-  storageBucket: "nolan-clone.firebasestorage.app",
-  messagingSenderId: "175935107884",
-  appId: "1:175935107884:web:7ad41df4eb6851f5abf289",
-  measurementId: "G-GB84NEB95P"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+
+
+  
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
