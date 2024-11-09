487db18281f7bebbc736885b3e470e6eb0dbeddb
Saraphina-babz <oyeyemibunmi21@gmail.com>
2024-11-09 23:38:01 +0100

    troubleshooting

--- NolanClone/src/components/Home.jsx
+++ NolanClone/src/components/Home.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+// import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
+    <div>
     <section className="home">
       <h1>Bring your film project to life from
       idea to production</h1>
       <button className='btn'><Link to='/script'>Create Script</Link></button>
       
     </section>
+    </div>
   );
 };
 
